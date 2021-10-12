8e16f9cca7c224787d4d51a23ba8e78b4e4ae39d
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-10-11 21:47:59 -0400

    arrays
    update

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -2,6 +2,7 @@
 const addResults = document.querySelector(".showResults");
 const printSalary = document.querySelector(".showSalary");
 const divContainer = document.getElementById("div-container");
+const divContainerTable = document.getElementById("results_table");
 
 // ARRAYS
 const person = [];
@@ -11,19 +12,26 @@ const salaries = [];
 addResults.addEventListener("click", displayResults);
 printSalary.addEventListener("click", displaySalary);
 
+// GLOVAL VARIABLES
+
 // FUNCTIONS
 
 function addSalary() {
   var userName = document.getElementById("employee-list").value;
   var userSalary = prompt("Enter the salary for " + userName + ":");
   person.push(userName);
-  salaries.push(userSalary);
+  salaries.push(parseFloat(userSalary));
   console.log(person);
   console.log(salaries);
   console.log("The username " + person + "salary is: " + userSalary);
 }
 
-function displayResults(salaries) {
+function displayResults() {
+  // AVERAGE
+  let total = salaries.reduce((sum, currentIndex) => sum + currentIndex, 0);
+  let averageSalary = parseFloat(total / salaries.length).toFixed(2);
+  let highestSalary = parseFloat(Math.max.apply(null, salaries));
+
   const results = document.createElement("div");
   results.classList.add("div-style");
   divContainer.appendChild(results);
@@ -33,42 +41,38 @@ function displayResults(salaries) {
   results.appendChild(addText);
   results.appendChild(average);
   results.appendChild(highest);
-  // CALCULATING THE AVERAGE
-  addText.innerHTML = "Result";
-  highest.innerHTML = "Highest resolt from the array";
-  average.innerHTML = "Average resolt from the array is: " + avg;
-  var sum = 0;
-  salaries.forEach(function (item, index) {
-    sum += item;
-  });
-  const avg = sum / salary.lenght;
-  return avg;
-}
 
-let headers = ["Username", "Salary"];
+  addText.innerHTML = "Result";
+  highest.innerHTML = "Highest resolt from the array " + highestSalary;
+  average.innerHTML = "Average resolt from the array is: " + averageSalary;
+  console.log(averageSalary);
+}
 
 function displaySalary() {
-  let addTable = document.createElement("table");
-  let headerRow = document.createElement("th");
-
-  headers.forEach((headerText) => {
-    let header = document.createElement("th");
-    let addHeader = document.createTextNode(headerText);
-    header.appendChild(addHeader);
-    headerRow.appendChild(header);
-  });
-  addTable.appendChild(headerRow);
-
-  person.forEach((employee) => {
-    let row = document.createElement("tr");
-
-    Object.value(employee).forEach((text) => {
-      let cell = document.createElement("td");
-      let cellContent = document.createTextNode(text);
-      cell.appendChild(cellContent);
-      row.appendChild(cell);
-    });
-    addTable.appendChild(row);
-  });
-  //.appendChild(addTable);
+  const addTable = document.createElement("TABLE");
+  addTable.classList.add("div-style");
+  divContainerTable.appendChild(addTable);
+  console.log("Hello World");
+  alert("hi");
 }
+
+//   let headerRow = document.createElement("th");
+//   headers.forEach((headerText) => {
+//     let header = document.createElement("th");
+//     let addHeader = document.createTextNode(headerText);
+//     header.appendChild(addHeader);
+//     headerRow.appendChild(header);
+//   });
+//   addTable.appendChild(headerRow);
+//   person.forEach((employee) => {
+//     let row = document.createElement("tr");
+//     Object.value(employee).forEach((text) => {
+//       let cell = document.createElement("td");
+//       let cellContent = document.createTextNode(text);
+//       cell.appendChild(cellContent);
+//       row.appendChild(cell);
+//     });
+//     addTable.appendChild(row);
+//   });
+//   .appendChild(addTable);
+// }
