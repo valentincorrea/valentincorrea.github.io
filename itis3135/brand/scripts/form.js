81f6db6383a298228b86993b54a2084ee3ed8834
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-10-31 22:33:06 -0400

    brand
    Testing my brand

---
+++ itis3135/brand/scripts/form.js
@@ -0,0 +1,31 @@
+function displayUserInformation() {
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const telephoneNumber = document.getElementById("telephone").value;
+  const emailAddress = document.getElementById("email").value;
+  if (!firstName || !lastName || !telephoneNumber || !emailAddress) {
+    alert("You forgot to fill all the boxes");
+    return;
+  }
+
+  const container = document.getElementById("information-box");
+  const informationContainer = document.createElement("div");
+  informationContainer.classList.add("card");
+  const userInformation = document.createElement("h2");
+  const userName = document.createElement("p");
+  const userEmail = document.createElement("p");
+  const userTelephone = document.createElement("p");
+
+  //   // APPENDING THE INFORMATION
+  container.appendChild(informationContainer);
+  informationContainer.appendChild(userInformation);
+  informationContainer.appendChild(userName);
+  informationContainer.appendChild(userEmail);
+  informationContainer.appendChild(userTelephone);
+
+  //   // Appending the content
+  userInformation.innerHTML = "User Information";
+  userName.innerHTML = "First Name: " + firstName + " " + lastName;
+  userEmail.innerHTML = "Email: " + emailAddress;
+  userTelephone.innerHTML = "Telephone: " + telephoneNumber;
+}
