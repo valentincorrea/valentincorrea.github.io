0356b7c837aeddb62eb5188dd19d1c95e31f4c80
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-11-05 23:29:07 -0400

    Final Project
    Before validation

---
+++ itis3135/final-project/scripts/form.js
@@ -0,0 +1,34 @@
+// THIS FUNCTION TAKES THE USER'S INFORMATION AND
+// DISPLAYS THE INFORMATION ON TOP OF THE CONTACT FORM
+function displayUserInformation() {
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const telephoneNumber = document.getElementById("telephone").value;
+  const emailAddress = document.getElementById("email").value;
+
+  if (!firstName || !lastName || !telephoneNumber || !emailAddress) {
+    alert("You forgot to fill all the boxes");
+    return;
+  }
+
+  const container = document.getElementById("information-box");
+  const informationContainer = document.createElement("div");
+  informationContainer.classList.add("card");
+  const userInformation = document.createElement("h3");
+  const userName = document.createElement("p");
+  const userEmail = document.createElement("p");
+  const userTelephone = document.createElement("p");
+
+  //   // APPENDING THE INFORMATION
+  container.appendChild(informationContainer);
+  informationContainer.appendChild(userInformation);
+  informationContainer.appendChild(userName);
+  informationContainer.appendChild(userEmail);
+  informationContainer.appendChild(userTelephone);
+
+  //   // Appending the content
+  userInformation.innerHTML = "Thank you for being a valuable member";
+  userName.innerHTML = "First Name: " + firstName + " " + lastName;
+  userEmail.innerHTML = "Email: " + emailAddress;
+  userTelephone.innerHTML = "Telephone: " + telephoneNumber;
+}
---
+++ itis3135/final-project/scripts/script.js
@@ -0,0 +1,5 @@
+// LOADS THE HEADER AND FOOTER TEMPLATE
+$(function () {
+  $("#hero").load("components/header.html");
+  $("#my-footer").load("components/footer.html");
+});
