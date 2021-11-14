44de63e2cc2aa88ceff231ba68693b4055b21d49
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-11-14 13:31:12 -0500

    Correa-Activity 11
    Activity 11 before the validation links

---
+++ itis3135/activity11/correa-activity11.js
@@ -0,0 +1,14 @@
+// <!-- jQuery call to the accordion() method. -->
+$(document).ready(function () {
+  $("#tabs").tabs();
+});
+
+$(document).ready(function () {
+  $(function () {
+    $("#accordion").accordion({
+      active: false, // Removed the active class of the first panel
+      collapsible: true, // Sets the the behaviour of the panels to the be collapsible
+      heightStyle: "content", // Removes the default height and set the height to adjust base on the content
+    });
+  });
+});
