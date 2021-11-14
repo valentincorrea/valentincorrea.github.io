2825ecc306bf15534daa63e48c305389e07de1b3
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-11-13 19:42:13 -0500

    Calculator app
    Updating the calculator app

--- itis3135/scripts/calculator-try.js
+++ itis3135/scripts/calculator-try.js
@@ -1,139 +1,137 @@
-// SELECTORS
-const calculator = document.querySelector(".calculator");
-const keys = calculator.querySelector(".calculator_keys");
-const display = document.querySelector(".calculator_display");
+// CALCULATOR CLASS
+class Calculator {
+  //CREATING A CONSTRUCTOR
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+  // CLEAR FUNTION
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
 
-// EVENT LISTENERS
+  // DELETE FUNCTION
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
 
-keys.addEventListener("click", (e) => {
-  if (e.target.matches("button")) {
-    const key = e.target;
-    const action = key.dataset.action;
-    const keyContent = key.textContent;
-    const displayedNum = display.textContent;
-    const previousKeyType = calculator.dataset.previousKeyType;
+  // APPEND NUMBERS TO THE SCREEN
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
 
-    // Remove .is-depressed class from all keys
-    Array.from(key.parentNode.children).forEach((k) => k.classList.remove("is-depressed"));
-
-    if (!action) {
-      console.log("number key!");
-      calculator.dataset.previousKey = "number";
-      if (displayedNum === "0" || previousKeyType === "operator") {
-        display.textContent = keyContent;
-      } else {
-        display.textContent = displayedNum + keyContent;
-      }
+  // choose operation funtion
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
     }
-    if (action === "add" || action === "subtract" || action === "multiply" || action === "divide") {
-      console.log("operator key!");
-      key.classList.add("is-depressed");
-      // Add custom attribute
-      calculator.dataset.previousKeyType = "operator";
-      calculator.dataset.firstValue = displayedNum;
-      calculator.dataset.operator = action;
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
 
-      const firstValue = calculator.dataset.firstValue;
-      const operator = calculator.dataset.operator;
-      const secondValue = displayedNum;
-
-      if (firstValue && operator && previousKeyType !== "operator" && previousKeyType !== "calculate") {
-      } else {
-        calculator.dataset.firstValue = displayedNum;
-      }
-
-      // Note: It's sufficient to check for firstValue and operator because secondValue always exists
-      if (firstValue && operator && previousKeyType !== "operator") {
-        // Update calculated value as firstValue
-      } else {
-        // If there are no calculations, set displayedNum as the firstValue
-        calculator.dataset.firstValue = displayedNum;
-      }
-
-      key.classList.add("is-depressed");
-      calculator.dataset.previousKeyType = "operator";
-      calculator.dataset.firstValue = displayedNum;
-      calculator.dataset.operator = action;
+  // COMPUTE FUNCTION
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      default:
+        return;
     }
-    if (action === "decimal") {
-      console.log("decimal key!");
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
 
-      // display.textContent = displayedNum + ".";
-      // Do nothing if string has a dot
-      if (!displayedNum.includes(".")) {
-        display.textContent = displayedNum + ".";
-        if (!displayedNum.includes(".")) {
-          display.textContent = displayedNum + ".";
-        } else if (previousKeyType === "operator") {
-          display.textContent = "0.";
-        }
-        calculator.dataset.previousKey = "decimal";
-      }
+  // DISPLAY NUMBERS
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", { maximumFractionDigits: 0 });
     }
-
-    if (action === "clear") {
-      calculator.dataset.previousKeyType = "clear";
-      console.log("clear key!");
-      if (key.textContent === "AC") {
-        calculator.dataset.firstValue = "";
-        calculator.dataset.modValue = "";
-        calculator.dataset.operator = "";
-        calculator.dataset.previousKeyType = "";
-      } else {
-        key.textContent = "AC";
-      }
-      display.textContent = 0;
-      key.textContent = "AC";
-      calculator.dataset.previousKeyType = "clear";
-      if (action !== "clear") {
-        const clearButton = calculator.querySelector("[data-action=clear]");
-        clearButton.textContent = "CE";
-      }
-    }
-
-    if (action === "calculate") {
-      if (firstValue) {
-        if (previousKeyType === "calculate") {
-          firstValue = displayedNum;
-          secondValue = calculator.dataset.modValue;
-        }
-        display.textContent = calculator(firstValue, operator, secondValue);
-      }
-
-      // Set modValue attribute
-      calculator.dataset.modValue = secondValue;
-      calculator.dataset.previousKeyType = "calculate";
-      console.log("equal key!");
-      const firstValue = calculator.dataset.firstValue;
-      const operator = calculator.dataset.operator;
-      const secondValue = displayedNum;
-      display.textContent = calculator(firstValue, operator, secondValue);
-
-      if (displayedNum === "0" || previousKeyType === "operator" || previousKeyType === "calculate") {
-        display.textContent = keyContent;
-      } else {
-        display.textContent = displayedNum + keyContent;
-      }
-      calculator.dataset.previousKeyType = "number";
-    }
-
-    if (action === "decimal") {
-      if (!displayedNum.includes(".")) {
-        display.textContent = displayedNum + ".";
-      } else if (previousKeyType === "operator" || previousKeyType === "calculate") {
-        display.textContent = "0.";
-      }
-
-      calculator.dataset.previousKeyType = "decimal";
-
-      const calculate = (n1, operator, n2) => {
-        const firstNum = parseFloat(n1);
-        const secondNum = parseFloat(n2);
-        if (operator === "add") return firstNum + secondNum;
-        if (operator === "subtract") return firstNum - secondNum;
-        if (operator === "multiply") return firstNum * secondNum;
-        if (operator === "divide") return firstNum / secondNum;
-      };
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
     }
   }
+
+  // UPDATE THE DISPLAY
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
+    }
+  }
+}
+
+// SELECTING THE ELEMENTS BY DATA TYPE
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector("[data-previous-operand]");
+const currentOperandTextElement = document.querySelector("[data-current-operand]");
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+// BUTTON PRESS EVENT LISTENER
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+// OPERATIONS BUTTON EVENT LISTENER
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+// EQUALS BUTTON EVENT LISTENER
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+// CLEAR BUTTON EVENT LISTENER
+allClearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+// DELETE BUTTON EVENT LISTENER
+deleteButton.addEventListener("click", (button) => {
+  calculator.delete();
+  calculator.updateDisplay();
 });
