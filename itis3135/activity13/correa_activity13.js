601d4ccf42a11ab54b4567d716ecc05cbd6be661
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-11-21 14:02:27 -0500

    Update correa_activity13.js
    ...

--- itis3135/activity13/correa_activity13.js
+++ itis3135/activity13/correa_activity13.js
@@ -3,21 +3,29 @@ $(document).ready(function () {
     type: "get",
     url: "team.json",
     beforeSend: function () {
+      //
+      console.log("Before sending data");
       $("#team").html("Loading...");
     },
     timeout: 10000,
     error: function (xhr, status, error) {
+      //
+      console.log("Alert Error");
       alert("Error: " + xhr.status + " - " + error);
     },
     dataType: "JSON",
     success: function (data) {
+      //
+      console.log("JSON has been received");
       $("#team").html("");
       $(data)
         .find("management")
         .children()
         .each(function () {
-          var xmlDoc = JSON.parse($(this));
-          $("#team").append("<h3>" + xmlDoc.find("name").text() + "</h3>" + xmlDoc.find("title").text() + "<br>" + xmlDoc.find("bio").text() + "<br>");
+          //
+          console.log("Before storing the JSON to the variable");
+          var xmlDoc = $(this);
+          $("#team").append("<h3>" + JSON.parse(xmlDoc).find("name").text() + "</h3>" + JSON.parse(xmlDoc).find("title").text() + "<br>" + JSON.parse(xmlDoc).find("bio").text() + "<br>");
         });
     },
   });
