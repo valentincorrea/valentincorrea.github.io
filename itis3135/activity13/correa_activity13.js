836b273af853054e21426b3d92d37f911ec473c2
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-11-21 16:40:15 -0500

    Update correa_activity13.js
    SImple fix

--- itis3135/activity13/correa_activity13.js
+++ itis3135/activity13/correa_activity13.js
@@ -1,37 +1,47 @@
 $(document).ready(function () {
-  $.ajax({
-    url: "https://webpages.uncc.edu/ncorream/itis3135/activity13/team.json",
-    method: "GET",
-    beforeSend: function () {
-      //
-      console.log("Before sending data");
-      $("#team").html("Loading...");
-    },
-    timeout: 10000,
-    error: function (xhr, status, error) {
-      //
-      console.log("Alert Error");
-      alert("Error: " + xhr.status + " - " + error);
-    },
-    dataType: "JSON",
-    success: function (data) {
-      //
-      console.log("JSON has been received");
-      $("#team").html("");
-      $(data)
-        .find("management")
-        .children()
-        .each(function () {
-          //
-          console.log("Before storing the JSON to the variable");
-          var xmlDoc = $(this);
-          //
-          console.log(JSON.parse(xmlDoc));
-          $("#team").append("<h3>" + xmlDoc.name + "</h3>" + xmlDoc.title + "<br>" + xmlDoc.bio + "<br>");
-        });
-    },
+  $.getJSON("team.json", function (data) {
+    $.each(data, function () {
+      $.each(this, function (key, value) {
+        $("#team").append("Name: " + value.name + "<br>" + "Title: " + value.timeout + "<br>" + "Bio: " + value.bio + "<br><br>");
+      });
+    });
   });
 });
+
+// $(document).ready(function () {
+//   $.ajax({
+//     url: "https://webpages.uncc.edu/ncorream/itis3135/activity13/team.json",
+//     method: "GET",
+//     beforeSend: function () {
+//       //
+//       console.log("Before sending data");
+//       $("#team").html("Loading...");
+//     },
+//     timeout: 10000,
+//     error: function (xhr, status, error) {
+//       //
+//       console.log("Alert Error");
+//       alert("Error: " + xhr.status + " - " + error);
+//     },
+//     dataType: "JSON",
+//     success: function (data) {
+//       //
+//       console.log("JSON has been received");
+//       $("#team").html("");
+//       $(data)
+//         .find("management")
+//         .children()
+//         .each(function () {
+//           //
+//           console.log("Before storing the JSON to the variable");
+//           var xmlDoc = $(this);
+//           //
+//           console.log(JSON.parse(xmlDoc));
+//           $("#team").append("<h3>" + xmlDoc.name + "</h3>" + xmlDoc.title + "<br>" + xmlDoc.bio + "<br>");
+//         });
+//     },
+//   });
+// });
 
 // $(document).ready(function () {
 //   $.ajax({
