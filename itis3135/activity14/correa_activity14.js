f23809a77d00cf06bd180c6d150064120faa78d2
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-11-24 23:07:15 -0500

    test
    test

--- itis3135/activity14/correa_activity14.js
+++ itis3135/activity14/correa_activity14.js
@@ -2,17 +2,15 @@ $(document).ready(function () {
   $("a[title='chua']").click(function () {
     console.log("Clicked");
     $.getJSON("https://webpages.uncc.edu/ncorream/itis3135/activity14/json_files/chua.json", function (data) {
-      $.each(data, function (key, value) {
-        console.log(data);
-        // value.month = "";
-        // value.title = "";
-        // value.speaker = "";
-        // value.image = "";
-        $("#month").append(data.month);
-        $("#title").append(data.title);
-        $("#speaker").append(data.speaker);
-        $("#picture").append(data.image);
-      });
+      console.log(data);
+      data.month = "";
+      data.title = "";
+      data.speaker = "";
+      data.image = "";
+      $("#month").append(data.month);
+      $("#title").append(data.title);
+      $("#speaker").append(data.speaker);
+      $("#picture").append(data.image);
     });
   });
 
