30a78a0e2cd83a4708c702deafbfeb8056050670
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-11-26 18:30:38 -0500

    test
    test

--- itis3135/activity14/correa_activity14.js
+++ itis3135/activity14/correa_activity14.js
@@ -1,79 +1,42 @@
-$("#btn").click(function () {
-  $.getJSON("https://webpages.uncc.edu/ncorream/itis3135/activity13/team.json", function (data) {
-    console.log("test");
-  });
+$(document).ready(function () {
+  const speaker_toobin = document.querySelector("[title='toobin']");
+  const speaker_sorkin = document.querySelector("[title='sorkin']");
+  const speaker_chua = document.querySelector("[title='chua']");
+  const speaker_sampson = document.querySelector("[title='sampson']");
+  // const speaker_chua = document.getElementById("btn");
+  // $.getJSON("https://webpages.uncc.edu/ncorream/itis3135/activity14/json_files/chua.json", function (data) {
+  //   test();
+  //   //       $.each(this, function (key, value) {
+  //   //         $("#team").append("Name: " + value.name + "<br>" + "Title: " + value.timeout + "<br>" + "Bio: " + value.bio + "<br><br>");
+  //   //       });
+  //   //     });
+  // });
+  speaker_toobin.addEventListener("click", test1);
+  speaker_sorkin.addEventListener("click", test2);
+  speaker_chua.addEventListener("click", test3);
+  speaker_sampson.addEventListener("click", test4);
+
+  function test3() {
+    console.log("test 1");
+    $.getJSON("https://webpages.uncc.edu/ncorream/itis3135/activity14/json_files/chua.json", function (obj) {
+      person = obj.speakers;
+      $("h1").empty();
+      $("h2").empty();
+      $("h3").empty();
+      $("#text").empty();
+      $("h1").append(person.title);
+      $("h2").append(person.month);
+      $("h3").append(pearson.speaker);
+      $("#text").append(person.text);
+    });
+  }
+  function test2() {
+    console.log("test 2");
+  }
+  function test3() {
+    console.log("test 3");
+  }
+  function test4() {
+    console.log("test 4");
+  }
 });
-
-// function switchSpeaker() {
-// $.getJSON("https://webpages.uncc.edu/ncorream/itis3135/activity14/json_files/tobbin.json", function (data) {
-//   $("#test").append(data.month);
-
-// speaker_tobbin = document.addEventListener("click", function () {
-//   $.getJSON("https://webpages.uncc.edu/ncorream/itis3135/activity14/json_files/tobbin.json",
-//   method: "GET",  function (data) {
-//     console.log(data);
-//     switch_speaker = data.speaker;
-//     switch_speaker = data.month = "";
-//     switch_speaker = data.title = "";
-//     switch_speaker = data.speaker = "";
-//     switch_speaker = data.image = "";
-//     $("#month").append(switch_speaker.month);
-//     $("#title").append(switch_speaker.title);
-//     $("#speaker").append(switch_speaker.speaker);
-//     $("#picture").append(switch_speaker.image);
-//     $("#picture").append(data.image);
-//   });
-// });
-// speaker_sorkin = document.addEventListener("click", function () {
-//   $.getJSON("https://webpages.uncc.edu/ncorream/itis3135/activity14/json_files/sorkin.json",
-//   method: "GET",function (data) {
-//     console.log(data);
-//     switch_speaker = data.speaker;
-//     switch_speaker = data.month = "";
-//     switch_speaker = data.title = "";
-//     switch_speaker = data.speaker = "";
-//     switch_speaker = data.image = "";
-//     $("#month").append(switch_speaker.month);
-//     $("#title").append(switch_speaker.title);
-//     $("#speaker").append(switch_speaker.speaker);
-//     $("#picture").append(switch_speaker.image);
-//   });
-// });
-// speaker_chua = document.addEventListener("click", function () {
-//   $.getJSON("https://webpages.uncc.edu/ncorream/itis3135/activity14/json_files/chua.json", function (data) {
-//     console.log(data);
-//     switch_speaker = data.speaker;
-//     switch_speaker = data.month = "";
-//     switch_speaker = data.title = "";
-//     switch_speaker = data.speaker = "";
-//     switch_speaker = data.image = "";
-//     $("#month").append(switch_speaker.month);
-//     $("#title").append(switch_speaker.title);
-//     $("#speaker").append(switch_speaker.speaker);
-//     $("#picture").append(switch_speaker.image);
-//   });
-// });
-// speaker_sampson = document.addEventListener("click", function () {
-//   $.getJSON("https://webpages.uncc.edu/ncorream/itis3135/activity14/json_files/sampson.json", function (data) {
-//     console.log(data);
-//     switch_speaker = data.speaker;
-//     switch_speaker = data.month = "";
-//     switch_speaker = data.title = "";
-//     switch_speaker = data.speaker = "";
-//     switch_speaker = data.image = "";
-//     $("#month").append(switch_speaker.month);
-//     $("#title").append(switch_speaker.title);
-//     $("#speaker").append(switch_speaker.speaker);
-//     $("#picture").append(switch_speaker.image);
-//   });
-// });
-
-//   $.getJSON("https://webpages.uncc.edu/ncorream/itis3135/activity14/json_files/toobin.json", function (data) {
-//     $.each(data, function () {
-//       $.each(this, function (key, value) {
-//         $("#team").append("Name: " + value.name + "<br>" + "Title: " + value.timeout + "<br>" + "Bio: " + value.bio + "<br><br>");
-//       });
-//     });
-//   });
-//}); // end ready
-//
