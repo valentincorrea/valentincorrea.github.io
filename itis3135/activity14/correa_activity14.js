50c502c573982c2f044e3629338c72ffb9fdfeeb
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-11-26 20:43:15 -0500

    test
    test

--- itis3135/activity14/correa_activity14.js
+++ itis3135/activity14/correa_activity14.js
@@ -74,6 +74,6 @@ $(document).ready(function () {
     $("h2").append(person.month);
     $("h3").append(person.speaker);
     $("#text").append(person.text);
-    document.getElementById("main_content").innerHTML = <img src="https://webpages.uncc.edu/ncorream/itis3135/activity14/${person.image}" alt="${person.speaker}_picture" />;
+    document.getElementById("main_content").innerHTML = '<img src="https://webpages.uncc.edu/ncorream/itis3135/activity14/${person.image}" alt="${person.speaker}_picture" />';
   }
 });
