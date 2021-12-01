c9a29958992a2cc615cb000bfe074ae24b124671
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-01 07:44:23 -0500

    activity
    test

--- itis3135/activity16/correa_activity16.js
+++ itis3135/activity16/correa_activity16.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  var url = "http://api.flickr.com/services/feeds/photos_public.gne?" + "id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+  var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
   $.getJSON(url, function (data) {
     var html = "";
     $.each(data.items, function (i, item) {
