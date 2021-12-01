225ab1f5da474f1910e7e4cf0e4753c1693e9c07
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-11-30 22:25:36 -0500

    activity15
    test

--- itis3135/activity15/correa_activity15.js
+++ itis3135/activity15/correa_activity15.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
+  $.ajax({
+    type: "get",
+    url: "facultyList.json",
+    beforeSend: function () {
+      $("#team").html("Loading...");
+    },
+    timeout: 10000,
+    error: function (xhr, status, error) {
+      alert("Error: " + xhr.status + " - " + error);
+    },
+    dataType: "json",
+    success: function (data) {
+      $("faculty").html("");
+      $.each(data, function () {
+        $.each(this, function (key, value) {
+          $("#faculty").append("<img src= '" + value.image + " '>" + "<br>" + value.full_name + "<br>" + value.deparment + "<br>" + value.bio + "<br>" + "<hr>");
+        });
+      });
+    },
+  });
 });
