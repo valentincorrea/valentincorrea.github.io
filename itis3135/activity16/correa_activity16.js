1f41728c02f21a00421960a403f0d717a7aefb16
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-01 07:33:30 -0500

    activity 16
    test

--- itis3135/activity16/correa_activity16.js
+++ itis3135/activity16/correa_activity16.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
+  var url = "http://api.flickr.com/services/feeds/photos_public.gne?" + "id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+  $.getJSON(url, function (data) {
+    var html = "";
+    $.each(data.items, function (i, item) {
+      html += "<h2>" + item.title + "</h2>";
+      html += "<p>" + item.content + "</p>";
+      html += "<img src=" + item.media.m + ">";
+    });
+    $("#new_building").html(html);
+  });
 });
