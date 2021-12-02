66eac6a1fad4ab64fd66a56b4371e26eee012227
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-01 21:57:39 -0500

    Update correa_activity16.js
    test

--- itis3135/activity16/correa_activity16.js
+++ itis3135/activity16/correa_activity16.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     var lightbox = "";
     $.each(data.items, function (i, item) {
       //   html += "<a data-lightbox='new_building'>" + "<img src=" + item.media.m + ">" + "</a>";
-      lightbox += "<a href='item.media.m' data-lightbox='new_building' data-tittle='item.title'>" + "<img src=" + item.media.m + ">" + "</a>";
+      lightbox += "<a href='item.media.m' data-lightbox='new_building' data-tittle='item.title'>" + "<img src=item.media.m>" + "</a>";
       //   lightbox += '<a href="${item.media.m}" data-lightbox="new_building" data-title="${item.title}"> <img src="$(item.media.m}"></a>';
     });
     $("#new_building").html(lightbox);
