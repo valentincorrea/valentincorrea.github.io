5aed0b70ea7d13c40a7a81e322465d6edf0aee75
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-01 22:35:13 -0500

    Update correa_activity16.js
    test

--- itis3135/activity16/correa_activity16.js
+++ itis3135/activity16/correa_activity16.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
   var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
   $.getJSON(url, function (data) {
-    var lightbox = document.getElementById("new_building");
+    var html = "";
     $.each(data.items, function (i, item) {
-      //   html += "<a data-lightbox='new_building'>" + "<img src=" + item.media.m + ">" + "</a>";
-      lightbox += "<a data-lightbox='new_building'>" + "<img src=" + item.media.m + ">" + "</a>";
+      html += "<a data-lightbox='new_building' data-title='item.title'>" + "<img src=" + item.media.m + ">" + "</a>";
     });
-    // $("#new_building").html(lightbox);
+    $("#new_building").html(html);
   });
 });
