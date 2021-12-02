f64e9c2a8837a74e10cc9be1c6ec6661511f688c
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-01 20:12:00 -0500

    Update correa_activity16.js
    test

--- itis3135/activity16/correa_activity16.js
+++ itis3135/activity16/correa_activity16.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
   var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
   $.getJSON(url, function (data) {
-    $("#new_building") = "";
+    var html = "";
     $.each(data.items, function (i, item) {
-      //   html += "<a data-lightbox='roadtrip'>" + "<img src=" + item.media.m + ">" + "</a>";
-      $("#new_building").innerHTML = '<a href="${item.media.m}" data-lightbox="new_building" item-title="${item.title}"> <img src="${item.media.m}"></a>';
+      html += "<a data-lightbox='new_building'>" + "<img src=" + item.media.m + ">" + "</a>";
     });
     $("#new_building").html(html);
   });
