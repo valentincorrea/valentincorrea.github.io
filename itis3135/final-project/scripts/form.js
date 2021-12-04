b5a06662e29c26b0907c52223e5e43ef824eb045
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-03 19:10:27 -0500

    final project update
    adding a faq accordion

--- itis3135/final-project/scripts/form.js
+++ itis3135/final-project/scripts/form.js
@@ -1,3 +1,14 @@
+// Accordion Jquery UI
+$(document).ready(function () {
+  $(function () {
+    $("#accordion").accordion({
+      active: false, // Removed the active class of the first panel
+      collapsible: true, // Sets the the behaviour of the panels to the be collapsible
+      heightStyle: "content", // Removes the default height and set the height to adjust base on the content
+    });
+  });
+});
+
 // THIS FUNCTION TAKES THE USER'S INFORMATION AND
 // DISPLAYS THE INFORMATION ON TOP OF THE CONTACT FORM
 function displayUserInformation() {
