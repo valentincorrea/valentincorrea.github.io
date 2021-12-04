5c5d9a08b5a1178251166aa0a7b7e52cc9aa196e
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-04 15:28:10 -0500

    test
    test

--- itis3135/final-project/scripts/form.js
+++ itis3135/final-project/scripts/form.js
@@ -1,42 +1,47 @@
 // Accordion Jquery UI
 // THIS FUNCTION TAKES THE USER'S INFORMATION AND
 // DISPLAYS THE INFORMATION ON TOP OF THE CONTACT FORM
-$(document).ready(function () {
-  $("#form-btn").click(function () {
-    const firstName = document.getElementById("first-name").value;
-    const lastName = document.getElementById("last-name").value;
-    const telephoneNumber = document.getElementById("telephone").value;
-    const emailAddress = document.getElementById("email").value;
-    console.log("Button press");
+// $(document).ready(function () {
+function submitForm() {}
+{
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const telephoneNumber = document.getElementById("telephone").value;
+  const emailAddress = document.getElementById("email").value;
+  console.log("Button press");
 
-    if (!firstName || !lastName || !telephoneNumber || !emailAddress) {
-      alert("You forgot to fill all the boxes");
-      return;
-    }
+  if (!firstName || !lastName || !telephoneNumber || !emailAddress) {
+    alert("You forgot to fill all the boxes");
+    return;
+  }
 
-    const container = document.getElementById("information-box");
-    const informationContainer = document.createElement("div");
-    informationContainer.classList.add("card");
-    const userInformation = document.createElement("h3");
-    const userName = document.createElement("p");
-    const userEmail = document.createElement("p");
-    const userTelephone = document.createElement("p");
+  const containerForm = document.getElementById("information-box");
+  const informationContainer = document.createElement("div");
+  informationContainer.classList.add("card");
+  const userInformation = document.createElement("h3");
+  const userName = document.createElement("p");
+  const userEmail = document.createElement("p");
+  const userTelephone = document.createElement("p");
 
-    //   // APPENDING THE INFORMATION
-    container.appendChild(informationContainer);
-    informationContainer.appendChild(userInformation);
-    informationContainer.appendChild(userName);
-    informationContainer.appendChild(userEmail);
-    informationContainer.appendChild(userTelephone);
+  //   // APPENDING THE INFORMATION
+  containerForm.appendChild(informationContainer);
+  informationContainer.appendChild(userInformation);
+  informationContainer.appendChild(userName);
+  informationContainer.appendChild(userEmail);
+  informationContainer.appendChild(userTelephone);
 
-    //   // Appending the content
-    userInformation.innerHTML = "Thank you for being a valuable member";
-    userName.innerHTML = "First Name: " + firstName + " " + lastName;
-    userEmail.innerHTML = "Email: " + emailAddress;
-    userTelephone.innerHTML = "Telephone: " + telephoneNumber;
-    alert("Submitted");
-  });
-});
+  //   // Appending the content
+  userInformation.innerHTML = "Thank you for being a valuable member";
+  userName.innerHTML = "First Name: " + firstName + " " + lastName;
+  userEmail.innerHTML = "Email: " + emailAddress;
+  userTelephone.innerHTML = "Telephone: " + telephoneNumber;
+  alert("Submitted");
+  document.getElementById("first-name").value = "";
+  document.getElementById("last-name").value = "";
+  document.getElementById("telephone").value = "";
+  document.getElementById("email").value = "";
+}
+// });
 
 // function displayUserInformation() {
 //   const firstName = document.getElementById("first-name").value;
