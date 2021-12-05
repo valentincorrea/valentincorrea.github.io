fa32fa0a66576633dde8d414524d133c94a07fbf
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-04 19:03:12 -0500

    test
    test

--- itis3135/final-project/scripts/form.js
+++ itis3135/final-project/scripts/form.js
@@ -17,8 +17,8 @@ function submitForm() {
   const containerForm = document.getElementById("information-box");
   const informationContainer = document.createElement("div");
   informationContainer.classList.add("card");
-  const userInformation = document.createElement("h3");
-  const userName = document.createElement("p");
+  const userInformation = document.createElement("h4");
+  const userName = document.createElement("h4");
   const userEmail = document.createElement("p");
   const userTelephone = document.createElement("p");
 
@@ -30,11 +30,11 @@ function submitForm() {
   informationContainer.appendChild(userTelephone);
 
   //   // Appending the content
-  userInformation.innerHTML = "Thank you for being a valuable member";
-  userName.innerHTML = "First Name: " + firstName + " " + lastName;
-  userEmail.innerHTML = "Email: " + emailAddress;
-  userTelephone.innerHTML = "Telephone: " + telephoneNumber;
-  alert("Submitted");
+  // userName.innerHTML = "Hi " + firstName + " " + lastName;
+  userInformation.innerHTML = "Hi " + firstName + " " + lastName + "! Thank you for being a valuable member";
+
+  // userEmail.innerHTML = "Email: " + emailAddress;
+  // userTelephone.innerHTML = "Telephone: " + telephoneNumber;
   document.getElementById("first-name").value = "";
   document.getElementById("last-name").value = "";
   document.getElementById("telephone").value = "";
