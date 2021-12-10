0399ca609bce9fc81dc3ccaa06acc38ad78a9b38
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-10 14:54:14 -0500

    Final project
    removing the final project

--- itis3135/final-project/scripts/accordion.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(function () {
-    $("#accordion").accordion({
-      active: false, // Removed the active class of the first panel
-      collapsible: true, // Sets the the behaviour of the panels to the be collapsible
-      heightStyle: "content", // Removes the default height and set the height to adjust base on the content
-    });
-  });
-});
--- itis3135/final-project/scripts/script.js
+++
@@ -1,5 +0,0 @@
-// LOADS THE HEADER AND FOOTER TEMPLATE
-$(function () {
-  $("#hero").load("components/header.html");
-  $("#my-footer").load("components/footer.html");
-});
