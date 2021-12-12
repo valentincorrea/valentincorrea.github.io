0dcb9236defda29b88ff04e6e83b109e8ff15a84
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-11 21:23:43 -0500

    test

--- itis3135/project/scripts/comments.js
+++ itis3135/project/scripts/comments.js
@@ -28,14 +28,19 @@ $(document).ready(function () {
               "</p>" +
               "<select id='user-rating'>" +
               "<option value='1'>" +
+              "1" +
               "<option>" +
               "<option value='2'>" +
+              "2" +
               "<option>" +
               "<option value='3'>" +
+              "3" +
               "<option>" +
               "<option value='4'>" +
+              "4" +
               "<option>" +
               "<option value='5'>" +
+              "5" +
               "<option>" +
               "</select>"
           );
