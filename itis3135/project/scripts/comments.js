1a63addde8ac8798da28fd9b438a9147cb59ccc2
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-11 21:12:46 -0500

    test

--- itis3135/project/scripts/comments.js
+++ itis3135/project/scripts/comments.js
@@ -14,7 +14,26 @@ $(document).ready(function () {
       $("#users-comments").html("");
       $.each(data, function () {
         $.each(this, function (key, value) {
-          $("#users-comments").append("<h4>" + value.title + "</h4>" + "<h5>" + "@" + value.user_name + "</h5>" + "<p>" + value.comment + "</p>" + "<br>" + "<hr>");
+          //   $("#users-comments").append("<h4>" + value.title + "</h4>" + "<h5>" + "@" + value.user_name + "</h5>" + "<p>" + value.comment + "</p>" + "<br>" + "<hr>");
+          $("#users-comments").append(
+            "<h4>" +
+              value.title +
+              "</h4>" +
+              "<h5>" +
+              "@" +
+              value.user_name +
+              "</h5>" +
+              "<p>" +
+              value.comment +
+              "</p>" +
+              "<select id='user-rating'" +
+              "<option value='1'" +
+              "<option value='2'" +
+              "<option value='3'" +
+              "<option value='4'" +
+              "<option value='5'" +
+              "</select>"
+          );
         });
       });
     },
