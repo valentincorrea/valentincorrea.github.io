6b02a8d039b7335032690f91df5f5598ea384ae9
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-15 18:33:39 -0500

    test

--- itis3135/activity16/correa_activity16.js
+++ itis3135/activity16/correa_activity16.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-  var flickr_api = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+  const vectaPhotos = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
   $.ajax({
     type: "GET",
-    url: "flickr_api",
+    url: vectaPhotos,
     beforeSend: function () {
       $("#new_bulding").html("loading...");
     },
-    // timeout: 20000,
-    // error: function (xhr, status, error) {
-    //   alert("Error" + xhr.status + error);
-    // },
+    timeout: 20000,
+    error: function (xhr, status, error) {
+      alert("Error" + xhr.status + error);
+    },
     dataType: "json",
     success: function (data) {
       $("#new_building").html("");
