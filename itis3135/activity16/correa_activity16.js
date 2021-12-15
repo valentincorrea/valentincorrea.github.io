5897178cf11b2e5be32345471c4b4b1653f52904
valentincorrea <85128002+valentincorrea@users.noreply.github.com>
2021-12-15 16:28:09 -0500

    test

--- itis3135/activity16/correa_activity16.js
+++ itis3135/activity16/correa_activity16.js
@@ -1,10 +1,32 @@
+// $(document).ready(function () {
+//   var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+//   $.getJSON(url, function (data) {
+//     var html = "";
+//     $.each(data.items, function (i, item) {
+//       html.innerHTML += "<a data-lightbox='new_building' data-title='item.title'>" + "<img src=" + item.media.m + ">" + "</a>";
+//     });
+//     $("#new_building").html(html);
+//   });
+// });
+
 $(document).ready(function () {
-  var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
-  $.getJSON(url, function (data) {
-    var html = "";
-    $.each(data.items, function (i, item) {
-      html += "<a data-lightbox='new_building' data-title='item.title'>" + "<img src=" + item.media.m + ">" + "</a>";
-    });
-    $("#new_building").html(html);
+  var flickr_api = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+  $.ajax({
+    type: "GET",
+    url: "flickr_api",
+    beforeSend: function () {
+      $("#new_bulding").html = "loading...";
+    },
+    timeout: 10000,
+    error: function (xhr, status, error) {
+      alert("Error" + xhr.status);
+    },
+    dataType: "json",
+    success: function (data) {
+      $("#new_building").html = "";
+      $.each(data.items, function (i, item) {
+        $("#new_building").append("<a href='" + item.media.m + "' data-lightbox='new_building'" + "data-title'" + item.title + "' >" + "<img src='" + item.media.m + "'>" + "</a>");
+      });
+    },
   });
 });
